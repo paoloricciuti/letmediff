37adfeac89c4b2ee661717023697cdd6b862ff01
paoloricciuti <ricciutipaolo@gmail.com>
2026-05-28 10:17:46 +0200

    chore: add tracking in prod

--- apps/letmediff.it/src/routes/+layout.svelte
+++ apps/letmediff.it/src/routes/+layout.svelte
@@ -7,12 +7,20 @@
 	import '@fontsource/geist-mono/500.css';
 	import './global.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { dev } from '$app/environment';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	{#if !dev}
+		<script
+			defer
+			src="https://umami.ricciuti.app/script.js"
+			data-website-id="7be7e93b-d509-45ff-9374-e127ae10cd17"
+		></script>
+	{/if}
 </svelte:head>
 
 {@render children()}
